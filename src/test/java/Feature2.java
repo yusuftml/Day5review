e0c48fb6c572bcd2a87490190f00873baa84aa5b
yusuftml <yusuftemel355@gmail.com>
2020-10-14 18:58:22 +0200

    Awesome feature 2 more added

--- src/test/java/Feature2.java
+++ src/test/java/Feature2.java
@@ -1,6 +1,8 @@
 public class Feature2 {
     public static void main(String[] args) {
         System.out.println("Working on feature2");
+        System.out.println("Working on feature2 more");
+        System.out.println("Working on feature2 again");
 
     }
 }
