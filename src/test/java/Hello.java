f34b38d6a3f36a3fa2228a9b1ef0c2ff756baaec
yusuftml <72047902+yusuftml@users.noreply.github.com>
2020-10-14 21:02:57 +0200

    added more changes by co-worker

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -1,5 +1,5 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Still really awesome hello");// added comment
+        System.out.println("Still really MW COWORKER ADDED  awesome hello");// We are simulating some changes that my coworker did
     }
 }
