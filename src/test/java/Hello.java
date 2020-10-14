8bf60303bcab2cddb265f27a213b8492e64c31e3
yusuftml <yusuftemel355@gmail.com>
2020-10-14 21:08:15 +0200

    added comment on same line

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -1,5 +1,5 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Still really MW COWORKER ADDED  awesome hello");// We are simulating some changes that my coworker did
+        System.out.println("This is my own hello");// This is my local hello
     }
 }
