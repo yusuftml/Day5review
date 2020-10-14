00134a1d11255a0fbbf3f07a5f26a64aaf611ec6
yusuftml <yusuftemel355@gmail.com>
2020-10-14 19:06:55 +0200

    Awesome feature 2 more added changes to line 3

--- src/test/java/Feature2.java
+++ src/test/java/Feature2.java
@@ -4,5 +4,7 @@ public class Feature2 {
         System.out.println("Working on feature2 more");
         System.out.println("Working on feature2 again");
 
+        System.out.println("Adding some more line 3");
+
     }
 }
